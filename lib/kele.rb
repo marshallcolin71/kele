83f62f87ac3a23cb316f23d4846f293b6327fa44
Colin <marshallcolin71@gmail.com>
2018-04-26 21:20:57 -0400

    added error-handler for invalid user/pass

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,9 +8,10 @@ class Kele
   include Roadmap
   include Messages
 
-  def initialize
+  def initialize (email, password)
     @api_url = 'https://www.bloc.io/api/v1'
     response = self.class.post("#{@api_url}/sessions", body: {email: email, password: password})
+    raise "invalid email/pass" if response.code != 200
     @auth_token = response['auth_token']
   end
 
